4bf819974e8ecf62f56fe268750fdc6ab4889e8d
ChristopherL <anbchris@gmail.com>
2024-05-12 16:13:38 -0400

    test branch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import Textfield from './Textfield.js';
-import recognizeMic from 'watson-speech/speech-to-text/recognize-microphone';
-
+import recognizeMicrophone from 'watson-speech/speech-to-text/recognize-microphone';
 import './App.css';
 
 
 class App extends Component {
-   constructor() {
-    super()
-    this.state = {}
-   }
+    constructor(props) {
+        super(props);
+        this.state = {
+        //   stream: null,
+        //   listening: false
+        };
+      }
     
-
-    listenCLick = () => {
-        
-    }
+    //   listenClick = () => {
+    //     if (!this.state.listening) {
+    //       const stream = recognizeMicrophone({
+    //         access_token: 'YOUR_ACCESS_TOKEN',
+    //         objectMode: true,
+    //         format: false, // Let Watson handle the formatting
+    //       });
+    
+    //       stream.on('data', data => {
+    //         // Handle the streaming data
+    //         console.log('Streaming data:', data);
+    //       });
+    
+    //       stream.on('error', error => {
+    //         // Handle errors
+    //         console.error('Speech-to-text error:', error);
+    //       });
+    
+    //       this.setState({ stream, listening: true });
+    //     } else {
+    //       this.state.stream.stop();
+    //       this.setState({ stream: null, listening: false });
+    //     }
+    //   }
 
     render() {
         return (
